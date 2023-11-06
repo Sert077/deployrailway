3180dacce956d011c8723c7bee0bd5a26edbe1be
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-06 07:23:41 -0400

    Update create.blade.php

--- resources/views/comite/create.blade.php
+++ resources/views/comite/create.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ url('/comite')}}" method="post" enctype="multipart/form-data">
+<form action="{{ 'https://deployrailway-production-3bd5.up.railway.app'.('/comite')}}" method="post" enctype="multipart/form-data">
     @csrf
     @include('comite.form');
 
