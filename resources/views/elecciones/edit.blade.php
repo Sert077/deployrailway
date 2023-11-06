f3eac733c24b2d1c5630a2b0a5e2b55e2e0f4c15
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-06 07:30:55 -0400

    Update edit.blade.php

--- resources/views/elecciones/edit.blade.php
+++ resources/views/elecciones/edit.blade.php
@@ -1,6 +1,6 @@
 formulario de edicion de elecciones
 
-<form action="{{ url('/elecciones/'.$elecciones->id )}}" method="post" enctype="multipart/form-data">
+<form action="{{ 'https://deployrailway-production-3bd5.up.railway.app'.('/elecciones/'.$elecciones->id )}}" method="post" enctype="multipart/form-data">
 @csrf
 {{ method_field('PATCH') }}
 @include('elecciones.form');
