75bcfdb3551babe196abdedf3cb241ec1d92e38b
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-06 07:31:50 -0400

    Update create.blade.php

--- resources/views/frente/create.blade.php
+++ resources/views/frente/create.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ url('/frente')}}" method="post" enctype="multipart/form-data">
+<form action="{{ 'https://deployrailway-production-3bd5.up.railway.app'.('/frente')}}" method="post" enctype="multipart/form-data">
     @csrf
     @include('frente.form');
 
