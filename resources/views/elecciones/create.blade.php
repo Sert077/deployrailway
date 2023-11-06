6840e7db129ffec880d875a7fbc7da3094c591a8
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-06 07:30:41 -0400

    Update create.blade.php

--- resources/views/elecciones/create.blade.php
+++ resources/views/elecciones/create.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ url('/elecciones')}}" method="post" enctype="multipart/form-data">
+<form action="{{ 'https://deployrailway-production-3bd5.up.railway.app'.('/elecciones')}}" method="post" enctype="multipart/form-data">
     @csrf
     @include('elecciones.form');
 
