37f85517b4e5bc1c4698a971ec9de6dcb010bc57
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-06 07:32:56 -0400

    Update edit.blade.php

--- resources/views/jurados/edit.blade.php
+++ resources/views/jurados/edit.blade.php
@@ -1,6 +1,6 @@
 
 
-<form action="{{ url('/jurados/'. $jurados->id )}}" method="post" enctype="multipart/form-data">
+<form action="{{ 'https://deployrailway-production-3bd5.up.railway.app'.('/jurados/'. $jurados->id )}}" method="post" enctype="multipart/form-data">
 @csrf
 {{ method_field('PATCH') }}
 @include('jurados.form');
