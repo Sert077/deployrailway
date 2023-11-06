75dc8cbe3180ce1387a221a7edd87a5e33c09e7e
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-06 06:35:59 -0400

    Update edit.blade.php

--- resources/views/comunicado/edit.blade.php
+++ resources/views/comunicado/edit.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ url('/comunicados/'. $comunicado->id )}}" method="post" enctype="multipart/form-data">
+<form action="{{ 'https://deployrailway-production-3bd5.up.railway.app'('/comunicados/'. $comunicado->id )}}" method="post" enctype="multipart/form-data">
     @csrf
     {{ method_field('PATCH') }}
     @include('comunicado.form');
