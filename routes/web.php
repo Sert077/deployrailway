c1e9e23da9b3d9a6da7da85ad9f42df096e4ed38
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-17 13:29:53 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,8 @@ Route::resource('frente', 'FrenteController');
 
 Route::resource('comunicados', 'ComunicadoController');
 
+Route::resource('documentaciones', 'DocumentacionController');
+
 Route::resource('mesas', 'MesaController');
 
 Route::resource('jurados', 'JuradoController');
@@ -57,6 +59,8 @@ Route::get('/mesas/{id}/generate-jurados', 'MesaController@generateJurados')->na
 
 Route::get('/mesas/{id}/lista-jurados', 'MesaController@listaJurados');
 
+Route::get('/mesas/{id}/acta', 'MesaController@visualizaracta');
+
 Route::get('/jurados/{id}/edit', 'JuradoController@edit')->name('jurados.edit');
 
 Route::get('/elecciones/{id}/boleta', 'EleccionController@showBoleta')->name('elecciones.boleta');
@@ -64,6 +68,40 @@ Route::get('/elecciones/{id}/boleta', 'EleccionController@showBoleta')->name('el
 Route::get('/votantes/carga', 'VotanteController@showCarga')->name('votante.carga');
 
 Route::post('/votantes/importCsv', 'VotanteController@importCsv')->name('votantes.importCsv');
+
+Route::get('/elecciones/{id}/previsualizacion', 'EleccionController@mostrarPrevisualizacion')->name('elecciones.previsualizacion');
+
+Route::get('/elecciones/{id}/registrar-resultados', 'EleccionController@registroResultados')
+    ->name('elecciones.registrarResultados');
+
+Route::patch('/elecciones/{id}/guardarResultados', 'EleccionController@guardarResultados')->name('elecciones.guardarResultados');
+
+Route::get('/elecciones/{id}/editar-resultados', [EleccionController::class, 'editarRegistroResultados'])->name('elecciones.editarResultados');
+Route::patch('/elecciones/{id}/guardar-edicion-resultados', [EleccionController::class, 'guardarEdicionResultados'])->name('elecciones.guardarEdicionResultados');
+
+
+//Route::get('/registroResultados', function () {
+  //return view('elecciones.registroResultados');
+//});
+
+Route::get('/reporte', 'ReporteController@index');
+Route::get('/reporteGrafico/{id}', 'ReporteController@reporteGrafico');
+
+Route::get('/generar-backup', [EleccionController::class, 'generarBackup']);
+
+Route::get('/mesas/{id}/actapdf', 'MesaController@pdf')->name('mesas.actapdf');
+
+Route::get('/generar-pdf/{id}','EleccionController@generarPDF')->name('elecciones.pdf');
+
+
+
+
+
+
+
+
+
+
 
 
 
