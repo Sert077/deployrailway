c77e2c83e5e7c2c760d3c99a0adcaf45d6637dee
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-17 12:54:21 -0400

    Update create.blade.php

--- resources/views/documentacion/create.blade.php
+++ resources/views/documentacion/create.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ url('/documentaciones')}}" method="post" enctype="multipart/form-data">
+<form action="{{ https://deployrailway-production-3bd5.up.railway.app'.('/documentaciones')}}" method="post" enctype="multipart/form-data">
     @csrf
     @include('documentacion.form');
 
