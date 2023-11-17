b27e84cf951df9ace16a722d9d447c00c20eb16b
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-17 13:23:44 -0400

    Update lista-jurados.blade.php

--- resources/views/mesas/lista-jurados.blade.php
+++ resources/views/mesas/lista-jurados.blade.php
@@ -19,7 +19,7 @@
             <div><a href="{{ url('/') }}">TRIBUNAL ELECTORAL</a></div>
             
             <div><a href="{{ url('/') }}">UNIVERSITARIO</a></div>
-        </div>s
+        </div>
         <ul>
             <li></li><li></li>
             <li></li><li></li>
@@ -29,7 +29,7 @@
         <li><a href="{{ url('/') }}">Inicio</a></li>
             <li><a href="{{ url('/elecciones') }}">Elecciones</a></li>
             <li><a href="{{ url('/comunicados') }}">Comunicados</a></li>
-            <li><a href="#">Documentación</a></li>
+            <li><a href="{{ url('/documentaciones') }}">Documentación</a></li>
             {{-- <li><a href="#">Acerca de</a></li>
             <li><a href="#">Contactos</a></li> --}}
             <li><a href="#">Ingreso</a></li>
@@ -50,7 +50,9 @@
             @if (isset($elecciones))
                 {{ method_field('PATCH') }}
             @endif
+            @if(isset($eleccion))
             <h2 class="form-title1">{{ $eleccion->nombre }}</h2>
+            @endif
             <br><br>
             <h2 class="form-title">Lista de Jurados</h2>
 
@@ -919,7 +921,7 @@ td:first-child {
                         <th>Nombre</th>
                         <th>Apellido Paterno</th>
                         <th>Apellido Materno</th>
-                        <th>Tipo Jurado</th>
+                        <th>Cargo Jurado</th>
                         <th>Cambiar Jurado</th>
                     </tr>
                 </thead>
