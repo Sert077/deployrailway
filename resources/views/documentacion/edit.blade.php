b8c5a9225f70cd868db39f20002d095931c940f2
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-11-18 21:12:50 -0400

    Update edit.blade.php

--- resources/views/documentacion/edit.blade.php
+++ resources/views/documentacion/edit.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ https://deployrailway-production-3bd5.up.railway.app'.('/documentaciones/'. $documentacion->id )}}" method="post" enctype="multipart/form-data">
+<form action="{{ 'https://deployrailway-production-3bd5.up.railway.app'.('/documentaciones/'. $documentacion->id )}}" method="post" enctype="multipart/form-data">
     @csrf
     {{ method_field('PATCH') }}
     @include('documentacion.form');
