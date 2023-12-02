f22256ebe750b72b6945f40ad46bbe8e3aafabe5
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-12-02 00:14:28 -0400

    Update index.blade.php

--- resources/views/frente/index.blade.php
+++ resources/views/frente/index.blade.php
@@ -510,7 +510,7 @@ td:first-child {
         <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             Cerrar Sesión
         </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        <form id="logout-form" action="{{ 'https://deployrailway-production-3bd5.up.railway.app'.('/logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
     @else
@@ -592,7 +592,7 @@ td:first-child {
                                                
                                  
                                  {{-- Inicio Función borrar --}}
-                                    <form id="delete-form-{{ $frente->id }}" action="{{ url('/frente/' . $frente->id) }}" method="post" style="display: inline;">
+                                    <form id="delete-form-{{ $frente->id }}" action="{{ 'https://deployrailway-production-3bd5.up.railway.app'.('/frente/' . $frente->id) }}" method="post" style="display: inline;">
                                     @csrf
                                  {{ method_field('DELETE') }}
                                      <button class="buttons-dentro-tabla" title="Borrar Elección" onclick="return confirm ('Quieres borrar este frente?')">
