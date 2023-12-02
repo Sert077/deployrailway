eede3b120b008ede82a72b28704877f1867fc122
Sergio Veizaga Sanchez <39845939+Sert077@users.noreply.github.com>
2023-12-02 00:06:30 -0400

    Update previsualizacion.blade.php

--- resources/views/elecciones/previsualizacion.blade.php
+++ resources/views/elecciones/previsualizacion.blade.php
@@ -414,7 +414,7 @@
         <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             Cerrar Sesión
         </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        <form id="logout-form" action="{{ 'https://deployrailway-production-3bd5.up.railway.app'.('/logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
     @else
@@ -492,12 +492,12 @@
                  @if (isset($eleccion) && $eleccion->convocatoria)
                   <div style="display: flex; align-items: center;">
                   <p style="margin-bottom: 0; margin-right: 10px;">Nombre del archivo: {{ $eleccion->convocatoria }}</p>
-                    <a href="{{ asset('storage/' . $eleccion->convocatoria) }}" download="Convocatoria.pdf"> 
+                    <a href="{{ asset('https://deployrailway-production-3bd5.up.railway.app/storage/' . $eleccion->convocatoria) }}" download="Convocatoria.pdf"> 
                   <img src="{{ asset('/images/descargar.png') }}" alt="Botón Descargar PDF" class="boton_descargar" title="Descargar Convocatoria" style="cursor: pointer;">
                    </a>
                   </div>
                  @endif
-                 <embed src="{{ asset('storage/' . $eleccion->convocatoria) }}" type="">
+                 <embed src="{{ asset('https://deployrailway-production-3bd5.up.railway.app/storage/' . $eleccion->convocatoria) }}" type="">
                  <br><br>
                         
                     <h2 class="forms" style="color: rgba(4, 36, 60, 0.99); font-size: 20px;  font-weight: 400; word-wrap: break-word;" >Tipo de Eleccion:</h2>
